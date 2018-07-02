f1c06b45f040651b365e62711f95acd64b996991
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-02 20:28:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,14 @@ class Dog
   @@all = []
  def initialize(name)
    @name = name
+   @@all << name
  end
 
-  def self.clear_all
- end
  def self.all
    @name
  end
 
+ def self.clear_all
+ end
 
 end
